ef5416aa0b38a7ef22d153f7dfff6c8f22d04e3f
Jefferson <44975876+ZX-80@users.noreply.github.com>
2022-05-24 16:19:58 -0400

    further refactoring

--- NTSB_bot.py
+++ NTSB_bot.py
@@ -3,61 +3,69 @@
 
 """Post all new NTSB aviation accident database entries to a subreddit"""
 
+import csv
 import praw
 import pathlib
+import traceback
+import configparser
 import test_db_access as av_mdb
+
 from datetime import datetime
 
-verbose=True
-def print_debug(text):
-    global verbose
-    if verbose:
-        print(text)
+def load_id_database():
+    with open('id_database.csv', 'r') as csv_fp:
+        return list(csv.reader(csv_fp))[0]
 
-def save_id_database():
-    open('id_database.txt','w').write('\n'.join(id_database))
+def save_id_database(id_database):
+    with open('id_database.csv', 'w') as csv_fp:
+        csv.writer(csv_fp).writerow(id_database)
 
-def post_incident(document):
-    global subreddit
+def post_incident(document, subreddit):
     print(document.ntsb_no)
-    print_debug('    Submitting post')
+    print('    Submitting post')
     subreddit.submit(title=document.title, selftext=document.text)
-    print_debug('        Post Submitted successfully\n')
+    print('        Post Submitted successfully\n')
 
-login_file=open('login.txt','r')
-REDDIT_USERNAME = login_file.readline().strip('\n')
-REDDIT_PASS = login_file.readline().strip('\n')
-user_agent = login_file.readline().strip('\n')
-print_debug('Logging in as: '+REDDIT_USERNAME)
-r = praw.Reddit(user_agent = user_agent,client_id=login_file.readline().strip('\n'),client_secret=login_file.readline().strip('\n'),username=REDDIT_USERNAME,password=REDDIT_PASS)
-print_debug('    Login Succesful\n')
-subreddit_name=login_file.readline().strip('\n')
-subreddit = r.subreddit(subreddit_name)
-subscriber_count = subreddit.subscribers
-print('Subscriber Count[',subscriber_count,']')
-id_database=[]
-for line in open('id_database.txt','r'):
-    id_database.append(line.strip('\n'))
-titles=[]
-
-update_count=0
-for document in av_mdb.parse_events():
+def get_subreddit():
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.read("account.ini")
+    print(f'Logging in as: {config["ACCOUNT INFO"]["username"]}')
     try:
-        if document.event_id not in id_database:
-            post_incident(document)
-            id_database.append(document.event_id)
-            update_count+=1
-    except Exception as e: 
-        print(str(e))
-        print('Invalid data!')
-save_id_database()
+        reddit = praw.Reddit(
+            client_id=config["ACCOUNT INFO"]["client id"],
+            client_secret=config["ACCOUNT INFO"]["client secret"],
+            password=config["ACCOUNT INFO"]["password"],
+            user_agent=config["ACCOUNT INFO"]["user agent"],
+            username=config["ACCOUNT INFO"]["username"],
+        )
+        print('    Login Successful\n')
+        return reddit.subreddit(config["ACCOUNT INFO"]["subreddit name"])
+    except BaseException as err:
+        traceback.print_exception(err)
+        print('    Login Failed\n')
+        return None
 
-#update sidebar with new date
-time_string = datetime.now().strftime("%d/%m/%Y")
-subreddit.mod.update(description=subreddit.description[:-10]+time_string)
-update_count_text = f"Scan complete: Added {update_count} incidents!"
-print(update_count_text)
+if (subreddit := get_subreddit()) != None:
+    success = 0
+    fail = 0
+    id_database = load_id_database()
+    for document in av_mdb.parse_events():
+        try:
+            if document.event_id not in id_database:
+                post_incident(document, subreddit)
+                id_database.append(document.event_id)
+                success += 1
+        except BaseException as err:
+            traceback.print_exception(err)
+            fail += 1
+    save_id_database()
 
-#make a report to desktop
-with open(pathlib.Path.home() / "Desktop" / "NTSB_Report.txt", 'w') as report_file:
-    report_file.write(f"{update_count_text}\n{time_string}\nSubs: {subscriber_count}")
+    #update sidebar with new date
+    time_string = datetime.now().strftime("%d/%m/%Y")
+    subreddit.mod.update(description=subreddit.description[:-10]+time_string)
+    update_count_text = f"Scan complete: Added {success} incidents!"
+    print(update_count_text)
+
+    #make a report to desktop
+    with open(pathlib.Path.home() / "Desktop" / "NTSB_Report.txt", 'w') as report_file:
+        report_file.write(f"{update_count_text}\n{time_string}\nSubs: {subreddit.subscribers}")
