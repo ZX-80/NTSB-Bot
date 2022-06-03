dc4c6542b742fbad0b8fd1bb25b74d3076eb6226
Jefferson <44975876+ZX-80@users.noreply.github.com>
2022-06-03 18:22:07 -0400

    Add type hinting

--- avdata.py
+++ avdata.py
@@ -19,62 +19,42 @@ from urllib.parse import urljoin
 init(autoreset=True)
 requests.urllib3.disable_warnings(requests.urllib3.exceptions.InsecureRequestWarning)
 
-def list_zip_files():
-    """Fetch the names, urls, and dates of all files available.
+def list_zip_files() -> list[tuple[str, str, datetime]]:
+    """Fetch the names, urls, and dates of all files available."""
+    url = "https://data.ntsb.gov"
+    page = requests.get(urljoin(url, "avdata")).text
+    soup = BeautifulSoup(page, "html.parser")
 
-    Returns
-    -------
-    list of tuple of string and string and datetime.datetime
-        The file information for all zip files available.
-    """
-    url = 'https://data.ntsb.gov'
-    page = requests.get(urljoin(url, 'avdata')).text
-    soup = BeautifulSoup(page, 'html.parser')
-
-    dates = [parsedate(node.string) for node in soup.find_all('td') if node.get('id') == "fileDate"]
-    names = [node.string for node in soup.find_all('td') if node.get('id') == "fileName"]
-    urls = [urljoin(url, node.get('href')) for node in soup.find_all('a')]
+    dates = [parsedate(node.string) for node in soup.find_all("td") if node.get("id") == "fileDate"]
+    names = [node.string for node in soup.find_all("td") if node.get("id") == "fileName"]
+    urls = [urljoin(url, node.get("href")) for node in soup.find_all('a')]
     return zip(names, urls, dates)
 
-def get_download_bar(downloaded_bytes, total_bytes):
-    """Create a download bar string.
-
-    Parameters
-    ----------
-    downloaded_bytes : int
-        This is the amount of bytes download so far.
-    total_bytes : int
-        This is the file size.
-
-    Returns
-    -------
-    string
-        The download bar string.
-    """
+def get_download_bar(downloaded_bytes: int, total_bytes: int) -> str:
+    """Generate a download bar string."""
     bar_length = 50
     percentage = downloaded_bytes / total_bytes
-    bar_completed = '\N{full block}' * int(bar_length * percentage)
+    bar_completed = "\N{full block}" * int(bar_length * percentage)
     return f"\r   {percentage:>4.0%} |{bar_completed:<{bar_length}}| {downloaded_bytes}/{total_bytes}"
 
-def download_file(file_path, url):
+def download_file(destination_file_path: Path, url: str) -> int:
     """Download a file from url.
 
     Parameters
     ----------
-    file_path : pathlib.Path
+    destination_file_path
         This is the filename and location to store the file.
-    url : string
+    url
         This is the url to download the file from.
 
     Returns
     -------
-    int
-        The success of the request.
+    The success of the request.
     """
     response = requests.get(url, verify=False, stream=True)
     if response.ok:
-        total_bytes = response.headers.get('content-length')
-        with open(file_path, 'wb') as fp:
+        total_bytes = response.headers.get("content-length")
+        with open(destination_file_path, "wb") as fp:
             if total_bytes is None:
                 fp.write(response.content)
                 print(get_download_bar(1, 1))
@@ -89,21 +69,20 @@ def download_file(file_path, url):
         print(f"    Error {response.status_code}:\n{response.text}")
     return response.ok
 
-def unzip(file_path):
-    """Unzip file_path to retrieve the Microsoft Access 2000 MDB file.
-
-    Parameters
-    ----------
-    file_path : pathlib.Path
-        This is the file to unzip.
-    """
-    with zipfile.ZipFile(file_path, "r") as zip_fp:
+def unzip(file_path: Path):
+    """Unzip file_path to retrieve the Microsoft Access 2000 MDB file."""
+    with zipfile.ZipFile(file_path, 'r') as zip_fp:
         zip_fp.extractall("Aviation_Data") 
     os.remove(file_path)
 
-def update():
-    """Check for and download any new files for this month"""
-    month_short  = datetime.today().strftime("%b").upper()
+def update() -> list[Path]:
+    """Check for and download any new files for this month
+
+    Returns
+    -------
+    The list of all files with events from this month.
+    """
+    month_short  = datetime.today().strftime('%b').upper()
     file_pattern = re.compile(fr"((up[0-9][0-9]{month_short})|(avall))\.zip")
     records_path = Path(__file__).parent.resolve() / "Aviation_Data"
     records_path.mkdir(exist_ok=True)
